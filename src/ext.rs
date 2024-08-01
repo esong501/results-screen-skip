514a6bfeae0548d46743dd34d7563d4f68821f45
charlotte <charlottemackenzi@gmail.com>
2024-08-01 14:19:35 +1200

    Update to 13.0.2

---
+++ src/ext.rs
@@ -0,0 +1,106 @@
+use modular_bitfield::specifiers::*;
+use modular_bitfield::bitfield;
+
+/// Controller class used internally by the game
+#[allow(non_snake_case)]
+#[repr(C)]
+pub struct Controller {
+    pub vtable: *const u64,
+    pub current_buttons: ButtonBitfield,
+    pub previous_buttons: ButtonBitfield,
+    pub left_stick_x: f32,
+    pub left_stick_y: f32,
+    pub left_trigger: f32,
+    pub _left_padding: u32,
+    pub right_stick_x: f32,
+    pub right_stick_y: f32,
+    pub right_trigger: f32,
+    pub _right_padding: u32,
+    pub gyro: [f32; 4],
+    pub button_timespan: AutorepeatInfo,
+    pub lstick_timespan: AutorepeatInfo,
+    pub rstick_timespan: AutorepeatInfo,
+    pub just_down: ButtonBitfield,
+    pub just_release: ButtonBitfield,
+    pub autorepeat_keys: u32,
+    pub autorepeat_threshold: u32,
+    pub autorepeat_initial_press_threshold: u32,
+    pub style: ControllerStyle,
+    pub controller_id: u32,
+    pub primary_controller_color1: u32,
+    pub primary_controller_color2: u32,
+    pub secondary_controller_color1: u32,
+    pub secondary_controller_color2: u32,
+    pub led_pattern: u8,
+    pub button_autorepeat_initial_press: bool,
+    pub lstick_autorepeat_initial_press: bool,
+    pub rstick_autorepeat_initial_press: bool,
+    pub is_valid_controller: bool,
+    pub _xB9: [u8; 2],
+    pub is_connected: bool,
+    pub is_left_connected: bool,
+    pub is_right_connected: bool,
+    pub is_wired: bool,
+    pub is_left_wired: bool,
+    pub is_right_wired: bool,
+    pub _xC1: [u8; 3],
+    pub npad_number: u32,
+    pub _xC8: [u8; 8],
+}
+
+
+#[repr(C)]
+pub struct AutorepeatInfo {
+    field: [u8; 0x18],
+}
+
+/// Controller style declaring what kind of controller is being used
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+#[repr(u32)]
+pub enum ControllerStyle {
+    Handheld = 0x1,
+    DualJoycon = 0x2,
+    LeftJoycon = 0x3,
+    RightJoycon = 0x4,
+    ProController = 0x5,
+    DebugPag = 0x6, // I assume
+    GCController = 0x7,
+}
+
+/// Re-ordered bitfield the game uses for buttons
+#[bitfield]
+#[derive(Debug, Default, Copy, Clone)]
+#[repr(C)]
+pub struct ButtonBitfield {
+    pub dpad_up: bool,
+    pub dpad_right: bool,
+    pub dpad_down: bool,
+    pub dpad_left: bool,
+    pub x: bool,
+    pub a: bool,
+    pub b: bool,
+    pub y: bool,
+    pub l: bool,
+    pub r: bool,
+    pub zl: bool,
+    pub zr: bool,
+    pub left_sl: bool,
+    pub left_sr: bool,
+    pub right_sl: bool,
+    pub right_sr: bool,
+    pub stick_l: bool,
+    pub stick_r: bool,
+    pub plus: bool,
+    pub minus: bool,
+    pub l_up: bool,
+    pub l_right: bool,
+    pub l_down: bool,
+    pub l_left: bool,
+    pub r_up: bool,
+    pub r_right: bool,
+    pub r_down: bool,
+    pub r_left: bool,
+    pub real_digital_l: bool,
+    pub real_digital_r: bool,
+    pub unused: B2,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,50 @@
 #![feature(proc_macro_hygiene)]
 
-use skyline::{hook, install_hook};
-use skyline::nn::hid::NpadHandheldState;
-use skyline::nn::ro::LookupSymbol;
 
 use smash::app::{self, lua_bind::*};
+use rand::Rng;
+pub mod ext;
+use ext::Controller;
 
+static mut SHOULD_END_RESULT_SCREEN : bool = false;
 pub static mut FIGHTER_MANAGER_ADDR: usize = 0;
-pub static mut SHOULD_END_RESULT_SCREEN : bool = false;
 
-pub fn handle_get_npad_state_start(
-    state: *mut NpadHandheldState,
-    _controller_id: *const u32,
-) {
-    unsafe {
-        let mgr = *(FIGHTER_MANAGER_ADDR as *mut *mut app::FighterManager);
-        if FighterManager::is_result_mode(mgr) && FighterManager::entry_count(mgr) > 0 {
-            let actual_state = *state;
-            let update_count = (*state).updateCount;
-            // 0: A, 2: X (Right Joycon), 12: LeftDpad (Left Joycon)
-            // TODO: Above^, should we support separate joycons? 
-            let key_a: u64 = 0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0001;
-            let key_start: u64 = 0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0100_0000_0000;
-            if (actual_state.Buttons & key_start) != 0 {
-                SHOULD_END_RESULT_SCREEN = true;
-            }
-
-            if SHOULD_END_RESULT_SCREEN {
-                use rand::{self, Rng};
-                let mut rng = rand::thread_rng();
-                // Need to space apart A-presses so it does not seem like we are holding the button.
-                let n: u32 = rng.gen_range(0..3);
-                if n == 1 {
-                    (*state).Buttons |= key_a;
-                }
-            }
-        } 
-        
-        if FighterManager::entry_count(mgr) == 0 {
+#[skyline::hook(offset = 0x3664CC0)]
+unsafe fn process_inputs_handheld(controller: &mut Controller) {
+    let mgr = *(FIGHTER_MANAGER_ADDR as *mut *mut app::FighterManager);
+    let entry_count = FighterManager::entry_count(mgr);
+    if FighterManager::is_result_mode(mgr) && entry_count > 0 {
+        if ninput::any::is_press(ninput::Buttons::PLUS) {
+            SHOULD_END_RESULT_SCREEN = true;
+        }
+        if ninput::any::is_press(ninput::Buttons::B) {
             SHOULD_END_RESULT_SCREEN = false;
         }
+        if SHOULD_END_RESULT_SCREEN {
+            let mut rng = rand::thread_rng();
+            // Need to space apart A-presses so it does not seem like we are holding the button.
+            let n: u32 = rng.gen_range(0..3);
+            if n == 1 {
+                controller.current_buttons.set_a(true);
+                controller.just_down.set_a(true);
+            }
+        }
     }
-}
-
-#[allow(improper_ctypes)]
-extern "C" {
-    fn add_nn_hid_hook(callback: fn(*mut NpadHandheldState,*const u32));
+    if entry_count == 0 {
+        SHOULD_END_RESULT_SCREEN = false;
+    }
+    call_original!(controller);
 }
 
 #[skyline::main(name = "results-screen")]
 pub fn main() {
-    std::thread::sleep(std::time::Duration::from_secs(20)); //makes it not crash on startup with arcrop bc ???
-    println!("[results-screen] Installing hook...");
     unsafe {
-        LookupSymbol(
+        skyline::nn::ro::LookupSymbol(
             &mut FIGHTER_MANAGER_ADDR,
-            "_ZN3lib9SingletonIN3app14FighterManagerEE9instance_E\u{0}"
-                .as_bytes()
-                .as_ptr(),
+            "_ZN3lib9SingletonIN3app14FighterManagerEE9instance_E\u{0}".as_bytes().as_ptr(),
         );
-
-        if (add_nn_hid_hook as *const ()).is_null() {
-            panic!("The NN-HID hook plugin could not be found and is required to add NRO hooks. Make sure libnn_hid_hook.nro is installed.");
-        }
-        add_nn_hid_hook(handle_get_npad_state_start);
     }
+    skyline::install_hooks!(
+        process_inputs_handheld
+    );
 }
