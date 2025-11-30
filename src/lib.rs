f4d585b6164ff048351f0a4fc163ac4bfdd81fcd
esong501 <y0501esong@gmail.com>
2025-11-30 00:46:45 -0500

    documenting changes

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,11 @@ unsafe fn process_inputs_handheld(controller: &mut Controller) {
     let mgr = *(FIGHTER_MANAGER_ADDR as *mut *mut app::FighterManager);
     let entry_count = FighterManager::entry_count(mgr);
     if FighterManager::is_result_mode(mgr) && entry_count > 0 {
+        // needs both plus and minus so that players with single left joycon can skip results
         if ninput::any::is_press(ninput::Buttons::PLUS) || ninput::any::is_press(ninput::Buttons::MINUS) {
             SHOULD_END_RESULT_SCREEN = true;
         }
+        // needs to check dpad left and A to stop skipping the screen with single joycons. dpad left is left joycon, A is right joycon
         if ninput::any::is_press(ninput::Buttons::B) || 
         ((controller.style == ControllerStyle::LeftJoycon && ninput::any::is_press(ninput::Buttons::LEFT)) || 
         (controller.style == ControllerStyle::RightJoycon && ninput::any::is_press(ninput::Buttons::A))){
@@ -29,6 +31,8 @@ unsafe fn process_inputs_handheld(controller: &mut Controller) {
             // Need to space apart A-presses so it does not seem like we are holding the button.
             let n: u32 = rng.gen_range(0..3);
             if n == 1 {
+                // check controller styles to know if the user has a single joycon to change "A"
+                // because single joycon changes the orientation of buttons.
                 if controller.style == ControllerStyle::LeftJoycon {
                     controller.current_buttons.set_dpad_down(true);
                     controller.just_down.set_dpad_down(true);
